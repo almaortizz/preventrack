9f74b22dd1dc6b8a1e5caffe5ce71cb9207981cd
Leonel <leogarmor123@gmail.com>
2026-09-02 17:50:39 -0600

    Actualizar IP del backend en la app movil

--- app_movil/preventrack/lib/config/api_config.dart
+++ app_movil/preventrack/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
   // Para emulador Android usa 10.0.2.2, para dispositivo físico usa tu IP local
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://192.168.0.104/api';
 }
